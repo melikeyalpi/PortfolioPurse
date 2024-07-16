e57a3f87b99af1dc220ca12d84f54ff267d910ef
melikeyalpi <yalpimelike@gmail.com>
2024-07-17 01:59:28 +0300

    Created service of portfolio purse

---
+++ portfoliopurse-service-main/src/main/java/com/portfoliopurse/portfoliopurseservicemain/PortfoliopurseServiceMainApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliopurse.portfoliopurseservicemain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliopurseServiceMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfoliopurseServiceMainApplication.class, args);
+    }
+
+}
